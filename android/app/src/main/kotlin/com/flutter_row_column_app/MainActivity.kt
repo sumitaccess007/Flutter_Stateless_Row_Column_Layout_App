64d179c00cb06f1ff36c717a1d0d70c11795c1a1
Sumit Sharma <sumitaccess007@gmail.com>
2021-02-14 16:40:07 +0530

    Stateless Flutter Row Column Layout App

---
+++ android/app/src/main/kotlin/com/flutter_row_column_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.flutter_row_column_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart' show debugPaintSizeEnabled;
+
+void main(){
+  debugPaintSizeEnabled = true;
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Layout Demo',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Flutter Layout Demo'),
+        ),
+        body: Center(
+          child: buildRow()
+        ),
+      ),
+    );
+  }
+
+  Widget buildRow() =>
+      Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        Image.asset('images/pic1.jpg'),
+        Image.asset('images/pic2.jpg'),
+        Image.asset('images/pic3.jpg'),
+      ],
+    );
+
+
+}
